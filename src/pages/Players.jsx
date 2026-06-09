ad3f7b804b5823f2a864bb9fa453728c4518d7cb
dionisiomoreno <dionisiomoreno917@gmail.com>
2026-06-09 10:59:33 +0200

    Players.jsx

--- src/pages/Players.jsx
+++ src/pages/Players.jsx
@@ -6,6 +6,7 @@ import clsx from 'clsx'
 import jsPDF from 'jspdf'
 import autoTable from 'jspdf-autotable'
 import { format, differenceInDays } from 'date-fns'
+
 const ROLES = ['admin','mister','player_paid','player_volunteer']
 const ROLE_LABELS = { admin:'Società', mister:'Mister', player_paid:'Calciatore', player_volunteer:'Volontario' }
 const ROLE_COLORS = {
